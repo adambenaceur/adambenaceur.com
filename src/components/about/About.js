359a4815e1a419571f1f91cc70782d41e18eb788
adambenaceur <benaceur.adam@gmail.com>
2022-04-07 18:12:12 -0600

    improved css layout for the about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,16 +9,13 @@ function About() {
           <img src='assets/profile.jpg' alt='profile-photo'/>
         </div>
         
-        <div className='title'>
-          <h1 >Hi, I'm Adam !</h1>
+        <div className='about-me'>
+            <h1 >Hi, I'm Adam !</h1>
+          <p> I am nursing student at the <a>University of Calgary</a>.</p>
+          <p> I like learning about computer science topics such as logic, and programming paradigms. My
+            interests include web 3.0 and full stack developement. </p>
+          <p> Thanks for checking out my page, <a>have a great rest of your day! </a></p>
         </div>
-        <p> I am nursing student at the <a>University of Calgary</a>.</p>
-
-        <p> I like learning about computer science topics such as logic, and programming paradigms. My
-           interests include web 3.0 and full stack developement. </p>
-        
-        <p> Thanks for checking out my page, <a>have a great rest of your day! </a></p>
-
     </div>
   )
 }
