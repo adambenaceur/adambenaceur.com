cfbb55fba995bbacdf333c2cc3d8dad0004716c9
adambenaceur <benaceur.adam@gmail.com>
2022-04-09 13:43:24 -0600

    improved the contact page js / scss  on full screen

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,16 +4,19 @@ import './Contact.scss'
 function Contact() {
   return (
     <div className='contact' id='contact'>
-      <div className='left'>
-        <img src='assets/shake.svg' alt=''/>
-      </div>
-      <div className='right'>
-        <h2>Contact.</h2>
-        <form>
-          <input type='text' placeholder='Email'/>
-          <textarea placeholder='Message'></textarea>
-          <button type='submit'>Send</button>
-        </form>
+      <div className='contact-title'> <h1>Contact</h1> </div>
+      <div className='contact-section'>
+        <div className='contact-img'>
+          <img src='assets/shake.svg' alt=''/>
+        </div>
+        <div className='contact-form'>
+          
+          <form>
+            <input type='text' placeholder='Email'/>
+            <textarea placeholder='Message'></textarea>
+            <button type='submit'>Send</button>
+          </form>
+        </div>
       </div>
       
       </div>
