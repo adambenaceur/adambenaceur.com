e79165646f1c62f6d60cf60186c7e4195033f377
adambenaceur <benaceur.adam@gmail.com>
2022-04-20 11:18:37 -0600

    added functionality for emails to messages to my inbox via emailjs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import './Contact.scss'
+import emailjs from 'emailjs-com'
 
 function Contact() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1s6fscm', 'template_yr8ugqa', e.target, 'AcmjQAKUiLtUninFe')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
   return (
     <div className='contact' id='contact'>
       <div className='contact-title'> <h1>Contact</h1> </div>
@@ -11,9 +24,9 @@ function Contact() {
         </div>
         <div className='contact-form'>
           
-          <form>
-            <input type='text' placeholder='Email'/>
-            <textarea placeholder='Message'></textarea>
+          <form onSubmit={sendEmail}>
+            <input type='text' placeholder='Email' name='email'/>
+            <textarea placeholder='Message' name = 'message'></textarea>
             <button type='submit'>Send</button>
           </form>
         </div>
