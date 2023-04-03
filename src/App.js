cb87aa2d33d4d707077549b55ecb8577c5bc3ea5
Adam Benaceur <benaceur.adam@gmail.com>
2023-04-03 17:26:41 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Header/>
           <Navigation/>
           <About/>
-          <Portfolio/>
+//           <Portfolio/>
           <Skills/>
           <Tools />
           <Contact/>
